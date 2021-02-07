5f14750fed21e45dada267f66e395e9e69839e90
KamranV21 <kamranv21@gmail.com>
2021-02-07 14:27:09 +0400

    first commit

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
+import '../providers/cart.dart';
+
+class ProductItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<Cart>(context, listen: false);
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          leading: IconButton(
+            icon: Consumer<Product>(
+                builder: (ctx, product, _) => Icon(product.isFavorite
+                    ? Icons.favorite
+                    : Icons.favorite_border)),
+            onPressed: () {
+              product.toggleFavoriteStatus();
+            },
+            color: Theme.of(context).accentColor,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              cart.addItem(product.id, product.title, product.price);
+            },
+            color: Theme.of(context).accentColor,
+          ),
+          backgroundColor: Colors.black87,
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
